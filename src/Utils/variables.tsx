4b6ca20d440e4509c128975bf300b5fb395b730b
Entela Mema <entela599@gmail.com>
2025-04-10 22:04:44 +0200

    fix: pipeline

--- src/Utils/variables.tsx
+++ src/Utils/variables.tsx
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8080/api/v1/'
+export const API_URL = 'https://api.maturity-models.oups.net:8080/api/v1/'
 export const LOCAL_STORAGE_KEY = "maturityModels";
