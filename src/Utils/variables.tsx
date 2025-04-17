4187589e5bf233453ffb5ffb592770b5f2350e9d
Entela Mema <entela599@gmail.com>
2025-04-18 01:12:13 +0200

    refactor: url prod

--- src/Utils/variables.tsx
+++ src/Utils/variables.tsx
@@ -1,4 +1,4 @@
 //PROD
-//export const API_URL = 'https://api.maturity-models.oups.net/api/v1/'
+export const API_URL = 'https://api.maturity-models.oups.net/api/v1/'
 
-export const API_URL = 'http://localhost:8080/api/v1/'
+//export const API_URL = 'http://localhost:8080/api/v1/'
